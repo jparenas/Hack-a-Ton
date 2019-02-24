bf9739b8bd98e0479f1c4dde22ee274e0d504b2d
sonegishi <5055ngs@gmail.com>
2019-02-23 22:07:18 -0600

    Enable to show pics

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+<div id='container'>
+
+<div>
+
+<img id='destination_image' v-bind:src='imageUrl' width=750px/>
+
+</div>
+
+<div>
+<h2>{{destination}}</h2>
+</div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+	name: 'Card',
+	props:{
+		destination: String,
+		imageUrl: String,
+	},
+	data () {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style scopped>
+#container {
+background: GRAY;
+border-style:solid;
+}
+</style>
