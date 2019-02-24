5e70d9cf478fa175ce4c329b645f8e51b5efd0e2
sonegishi <5055ngs@gmail.com>
2019-02-24 12:12:36 -0600

    Modify some lines in main.js and App.vue

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import router from './router'
 
 import BootstrapVue from 'bootstrap-vue'
 import UUID from 'vue-uuid';
- 
+
+import App from './App'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 Vue.use(UUID);
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
